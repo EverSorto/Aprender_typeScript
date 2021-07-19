1d9a83e6f749d96845cd4b3aa9bd785c0f19c211
YonatanSeg <43020373+YonatanSeg@users.noreply.github.com>
2021-07-19 05:00:07 +0200

    compendiendo el codigo, me costo entender en enrutado, pero esta muy manejable con esta estructura. Commit para añadir description, initTime y lasttime en el modelo

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import 'source-map-support/register';
 import express from 'express';
 import mongoose from './services/mongoose';
 import { v1 } from './routes';
+import morgan from 'morgan'
 
 const port = process.env.APP_PORT || 3000;
 const mongoDB = process.env.APP_MONGODB_URI;
@@ -11,6 +12,12 @@ const app = express();
 
 // permite usar json en las peticiones
 app.use(express.json());
+
+// se añade un middleware para que express reconosca los datos enviados por post
+app.use(express.urlencoded({extended:true}))
+
+// morgan es para que la consola informe sobre las acciones del servidor 
+app.use(morgan("dev"))
 
 // cargamos las rutas
 app.use('/v1', v1);
@@ -20,9 +27,9 @@ mongoose.connect(mongoDB, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 }).catch(error => {
-	console.log(error);
+	console.log("error the connections => \n", error);
 });
-
+    
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
 });
--- src/routes/v1/todo/todo.route.ts
+++ src/routes/v1/todo/todo.route.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { createTodo, getAllTodos } from './todo.controller';
-
+ 
 const router = Router();
 
 // get and post function https://expressjs.com/es/4x/api.html#router.route
